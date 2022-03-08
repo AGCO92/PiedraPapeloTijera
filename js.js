bcf2dbd63ee6efcee864081017b724fca2e93d56
AGCO92 <aguscossio@live.com.ar>
2022-03-07 23:40:19 -0300

    Creates function computerPlays

--- js.js
+++ js.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+let playerSelection;
+
+function computerPlays() {
+    let computerSelection = Math.random(); 
+    if (computerSelection < 0.33) {
+        return 'Rock';
+    } else if (computerSelection >= 0.33 && computerSelection < 0.66) {
+        return 'Paper';
+    } else {
+        return 'Scissors'
+    }
+}
+
+playerSelection = prompt("What do you choose?", "");
+playerSelection = playerSelection.toLowerCase();
+
+function playRound (computerSelection, playerSelection) {
+    /*devuelve quien es el ganador "You Lose! Paper beats Rock"*/
+}
+
+function game (playRound, 5) {
+    /*arroja el resultado de quien gana*/
+}
