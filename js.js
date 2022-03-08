63d1e61817d6dec7074b1a6d9a772d06c1cee04f
AGCO92 <aguscossio@live.com.ar>
2022-03-08 02:47:06 -0300

    All done

--- js.js
+++ js.js
@@ -1,11 +1,22 @@
-let computerSelection = computerPlays();
-let playerSelection;
-let win = `You Win! ${playerSelection} beats ${computerSelection}`
-let lose = `You Lose! ${computerSelection} beats ${playerSelection}`
-let tie = `It's a tie`
+let playerCount = 0;
+let computerCount = 0;
+let win;
+let lose;
+let tie;
 
-playerSelection = prompt("What do you choose?", "");
-playerSelection = playerSelection.toLowerCase();
+
+function playRound (computerSelection, playerSelection) {    
+    win = `You Win! ${playerSelection} beats ${computerSelection}`;
+    lose = `You Lose! ${computerSelection} beats ${playerSelection};`
+    tie = `It's a tie`;
+    if (computerSelection === playerSelection) {
+        return tie;
+    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
+        return win;
+    } else {
+        return lose;
+    }
+}
 
 function computerPlays() {
     let choose = Math.random(); 
@@ -18,16 +29,34 @@ function computerPlays() {
     }
 }
 
-function playRound (playerSelection, computerSelection) {
-    if (computerSelection === playerSelection) {
-        return tie;
-    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
-        return win;
+function playerPlays() {
+    return prompt("What do you choose?", "").toLowerCase();
+}
+
+function logIt(resultado) {
+    console.log(resultado)
+}
+
+
+function game (playerPlays, computerPlays, playRound, logIt) {
+    for(let i = 0; i < 5; i++){
+        let playerSelection = playerPlays();
+        let computerSelection = computerPlays();
+        let result = playRound(playerSelection, computerSelection);
+        logIt(result)
+        if (win) {
+            playerCount++;
+        } else if (lose) {
+            computerCount++
+        }
+    }
+    if (playerCount < computerCount){
+        logIt('Congratulations, you win')
+    } else if (playerCount > computerCount) {
+        logIt('Sorry, you lose')
     } else {
-        return lose;
+        logIt("It's a tie")
     }
 }
- 
-function game (playRound, x) {
-    /*arroja el resultado de quien gana*/
-}
+
+game (playerPlays, computerPlays, playRound, logIt)
